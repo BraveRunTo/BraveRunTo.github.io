e3704ddf9f7fdfc3a6ed1576f03657b4d401c52d
BraveRunTo <1054773387@qq.com>
2024-07-18 00:21:21 +0800

    文章发布

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1720872059',resources: [
+    cacheName: 'chirpy-1721233223',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
