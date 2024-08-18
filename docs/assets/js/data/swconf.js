0369449ed115eddbbfd47526d0118caed383631c
BraveRunTo <1054773387@qq.com>
2024-08-18 23:47:25 +0800

    sass-converter 降低版本

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1722756216',resources: [
+    cacheName: 'chirpy-1723996012',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
